7c2b0b0b4ee25d3f23ec916528c0d0d1fd907661
Dominika Włodarczyk <dominikaa.99@gmail.com>
2020-11-26 23:50:34 +0100

    Add filtering options

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
+import {AnimatedSwitch} from 'react-router-transition';
 import PropTypes from 'prop-types';
+import styles from './App.scss';
 
 import MainLayout from './components/layout/MainLayout/MainLayout';
 
 import Home from './components/views/Home/Home';
 import Trips from './components/views/Trips/TripsContainer';
+import Countries from './components/views/Countries/CountriesContainer';
+import Regions from './components/views/Regions/RegionsContainer';
 // TODO - import other views
 import Info from './components/views/Info/Info';
 import NotFound from './components/views/NotFound/NotFound';
 
 import parseTrips from './utils/parseTrips';
 import {setMultipleStates} from './redux/globalRedux';
+import Country from './components/views/Country/CountryContainer';
+import Trip from './components/views/Trip/TripContainer';
 
 class App extends React.Component {
   static propTypes = {
@@ -32,18 +38,45 @@ class App extends React.Component {
       parseTrips(this.props.trips, this.props.setStates);
     }
   }
+  
+  mapStyles(styles) {
+    return {
+      opacity: styles.opacity,
+      transform: `translate(0,${styles.transform}px)`,
+    };
+  }
 
   render(){
     return (
       <BrowserRouter>
         <MainLayout>
-          <Switch location={location}>
+          <AnimatedSwitch 
+            // location={location}
+            atEnter={{
+              opacity: 0,
+              transform: 200,
+            }}
+            atLeave={{
+              opacity: 0,
+              transform: 0,
+            }}
+            atActive={{
+              opacity: 1,
+              transform: 0,
+            }}
+            mapStyles={this.mapStyles}
+            className={styles.switchWrapper}
+          >
             <Route exact path='/' component={Home} />
             <Route exact path='/trips' component={Trips} />
-            {/* TODO - add more routes for other views */}
+            <Route exact path='/countries' component={Countries} />
+            <Route exact path='/regions' component={Regions} />
             <Route exact path='/info' component={Info} />
+            <Route exact path='/country/:id' component={Country} />
+            <Route exact path='/trip/:id' component={Trip} />
+
             <Route path='*' component={NotFound} />
-          </Switch>
+          </AnimatedSwitch>
         </MainLayout>
       </BrowserRouter>
     );
