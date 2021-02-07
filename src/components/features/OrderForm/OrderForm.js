1806d3569193c556705b96ca67689b8dfd0f6574
Dominika Włodarczyk <dominikaa.99@gmail.com>
2021-02-07 22:03:34 +0100

    Add server files

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -4,10 +4,43 @@ import PropTypes from 'prop-types';
 import Section from '../../layout/Section/Section';
 
 import {Grid, Row, Col} from 'react-flexbox-grid';
+import Button from '../../common/Button/Button';
 
 import OrderSummary from '../OrderSummary/OrderSummary';
 import OrderOption from '../OrderOption/OrderOption';
 import pricing from '../../../data/pricing.json';
+
+import {formatPrice} from '../../../utils/formatPrice';
+import {calculateTotal} from '../../../utils/calculateTotal';
+
+import {settings} from '../../../data/settings';
+
+const sendOrder = (options, tripCost) => {
+  const totalCost = formatPrice(calculateTotal(tripCost, options));
+
+  const payload = {
+    ...options,
+    totalCost,
+  };
+
+  const url = settings.db.url + '/' + settings.db.endpoint.orders;
+
+  const fetchOptions = {
+    cache: 'no-cache',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  };
+
+  fetch(url, fetchOptions)
+    .then(function(response){
+      return response.json();
+    }).then(function(parsedResponse){
+      console.log('parsedResponse', parsedResponse);
+    });
+};
 
 const OrderForm = ({tripCost, setOrderOption, options}) => (
   <Section>
@@ -23,6 +56,7 @@ const OrderForm = ({tripCost, setOrderOption, options}) => (
         </Col>
       </Row>
     </Grid>
+    <Button onClick={() => sendOrder(options, tripCost)}>Order now!</Button>
   </Section>
 );
 
